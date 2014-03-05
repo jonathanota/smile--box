79a7f7d8641f7ab5d82eba53dfd7bd2b05df949d
JonathanOta <jonathanota1@gmail.com>
2014-03-04 17:16:07 -0800

    Initial Upload
    .pde and .ino files

---
+++ ServoKey/ServoKey.ino
@@ -0,0 +1,64 @@
+#include <Servo.h>
+#include <Boards.h>
+#include <Firmata.h>
+
+Servo servo;
+
+int pos = 0;
+int faceZeroValue;
+int reset = 155;
+boolean faceZero = true;
+//int key;
+
+void setup(){
+  pinMode(13, OUTPUT);
+ servo.attach(9);
+  Serial.begin(19200);
+  servo.write(reset);
+
+}
+
+void loop(){
+  if (Serial.available()){
+
+    int  key = Serial.read();
+    
+    if (key > 1){
+      if (faceZero){
+        faceZeroValue = key;
+        faceZero = false;
+      }
+    
+    int x = key;
+    
+    servo.write(map(x, faceZeroValue, faceZeroValue + 8, reset, 0));
+    }
+
+//    if (key == 'a'){
+//      digitalWrite(13, LOW);
+//       //servo.attach(9, 100, 150);
+//       servo.write(100);
+//    }
+//    if (key == 'b'){
+//     // servo.attach(9, 800, 544);
+//      //servo.detach();
+//      servo.write(180);
+//    }
+//
+    if (key == 1){
+      digitalWrite(13, HIGH); 
+    }
+    
+    if (key < 1){
+     digitalWrite(13, LOW); 
+     servo.write(reset);
+     faceZero = true;
+    }
+  }
+
+
+}
+
+
+
+
